c4746d6513a1bb67fef96387120052164fb79e6c
Taron <saribekyantaron1998@gmail.com>
2022-05-13 19:43:40 +0400

    add code

---
+++ ApplePie/Game.swift
@@ -0,0 +1,34 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Taron on 13.05.22.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_ "
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
